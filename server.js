955b6a6b752a3509298c8f1501113414a07c0047
Diego Burlando <diego@dmm888.com>
2017-12-27 22:29:24 +0100

    Added prerendering to static ssr

--- server.js
+++ server.js
@@ -1,17 +1,41 @@
 import express from 'express';
 import React from 'react';
 import ReactDOM from 'react-dom/server';
-import { renderToString } from 'react-dom/server';
+import { renderToString, renderToStaticMarkup } from 'react-dom/server';
 import About from './about';
 import fetch from 'isomorphic-fetch';
-import path from 'path'
+import path from 'path';
+import fse from 'fs-extra';
+
 const app = express();
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.use(express.static(__dirname + '/public'));
-console.log(__dirname)
+console.log(__dirname);
+
+let html = renderToStaticMarkup(<About/>)
+let template = `
+    <!doctype html>
+    <html>
+        <head>
+            <title>Example of universal app with react</title>
+             <link rel="stylesheet" href="./styles.css" asp-append-version="true" />
+             <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        </head>
+        <body>       
+          <div id='app'>${html}</div>
+          <script src='bundle.js'></script>
+        </body>
+    </html>
+`
+
+fse.outputFileSync('./dist/public/prerender.html', template);
+
+
+
+
 
 app.get('/', (req, res) => {   
 	res.send(`
@@ -28,5 +52,7 @@ app.get('/', (req, res) => {
         </body>
     </html>
 `);})
+
+
 app.listen(port, ip, () => console.log(`Example app listening on port ${port} and ${ip}`));
 
