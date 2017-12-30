2ff1734a22164365c0d0ad41617b30ef642ed35c
Diego Burlando <diego@dmm888.com>
2017-12-30 14:19:16 +0100

    customization

--- about.js
+++ about.js
@@ -22,8 +22,9 @@ render() {
               </div>
               <p onClick = {()=>{this.info()}}>Tech Lead</p>
               <hr/>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec dapibus metus, ac venenatis eros. Nulla iaculis luctus massa, fringilla tempus ligula lacinia at. Etiam tincidunt sit amet lectus non dignissim. Integer fermentum, massa quis dictum malesuada, purus purus vulputate urna, in gravida velit est accumsan massa. Cras eget ante nulla. Suspendisse ullamcorper, nisl id interdum laoreet, metus libero suscipit sem, non ultricies est tellus euismod ante. Maecenas commodo fringilla sapien, in hendrerit diam fermentum id. Mauris lobortis lacinia diam vel placerat. Aliquam condimentum dui id tristique placerat.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec dapibus metus, ac venenatis eros. Nulla iaculis luctus massa, fringilla tempus ligula lacinia at. Etiam tincidunt sit amet lectus non dignissim. Integer fermentum, massa quis dictum malesuada, purus purus vulputate urna, in gravida velit est accumsan massa. Cras eget ante nulla. Suspendisse ullamcorper, nisl id interdum laoreet, metus libero suscipit sem, non ultricies est tellus euismod ante. Maecenas commodo fringilla sapien, in hendrerit diam fermentum id. Mauris lobortis lacinia diam vel placerat. Aliquam condimentum dui id tristique placerat.</p>
+              <p>My passion for software development has led me to contribute to many different projects. I specialise in mentoring and motivating agile
+              teams and my presence hands on code facilitates the rapid delivery with best practices and solid principles.</p>
+              <p></p>
                     <h3>Interests & hobbies</h3>
               <ul>
                 <li>classic guitar playing</li>
